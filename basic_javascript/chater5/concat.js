b7084ffcf1dfb9e164d787eeed983636b830ccf1
stemkorea7 <66106442+stemkorea7@users.noreply.github.com>
2020-09-14 17:18:54 +0900

    Create concat.js

---
+++ basic_javascript/chater5/concat.js
@@ -0,0 +1,34 @@
+// //숫자 결합
+// const account1 = 500;
+// const account2 = 1000;
+// const allAccount = account1 + account2;
+
+// console.log(account1 + account2);
+// console.log(allAccount);
+
+// //String 합치기
+// const user = "George 님! ";
+// const greeting = "저희 사이트에 오신것을 환영합니다!";
+
+// console.log(user + greeting);
+
+// // 문자열 입력시 오류 상황
+// const greeting = "안녕하세요. "George" 입니다.";
+// const greeting = '안녕하세요. "George" 입니다.'; //' '사용
+// const greeting = '안녕하세요. "i'm George" 입니다.';
+// const greeting = `안녕하세요. i'm George" 입니다.`; //백틱 사용
+
+// console.log(greeting);
+
+// //백틱 ${ }활용
+// const user = "George";
+// const greeting = `안녕하세요. ${user} 입니다.`;
+
+// console.log(greeting);
+
+//숫자 문자 합칠때 차이
+console.log(1 + 1);
+console.log(1 + "1"); //문자로 인식
+console.log("George" + "kim");
+console.log("George" / 3); //NaN (Not a Number) x
+console.log("6" / 3); //숫자로 인식
