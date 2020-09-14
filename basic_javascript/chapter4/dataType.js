b442550097fbd32211f54d49e9eb4e51ac0da663
stemkorea7 <66106442+stemkorea7@users.noreply.github.com>
2020-09-14 13:40:26 +0900

    Create dataType.js

---
+++ basic_javascript/chapter4/dataType.js
@@ -0,0 +1,19 @@
+//숫자(Number)
+const age = 24;
+console.log(typeof age);
+
+//문자(String)
+const name = "George";
+console.log(typeof name);
+
+//참 또는 거짓(Booleans)
+const isLogin = true; //or false
+console.log(typeof isLogin);
+
+//값설정이 x(Undefined)
+let myAge;
+console.log(typeof myAge);
+
+//값이 x(Null)
+const myAge1 = null;
+console.log(myAge1);
