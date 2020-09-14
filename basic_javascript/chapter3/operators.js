4f20022899f3d0312f12dbc70ebb52ee6a916979
stemkorea7 <66106442+stemkorea7@users.noreply.github.com>
2020-09-14 13:06:52 +0900

    Create operators.js

---
+++ basic_javascript/chapter3/operators.js
@@ -0,0 +1,24 @@
+// 사칙연산과 제곱, 나머지값
+let age = 10;
+
+console.log(age + 5);
+console.log(age - 5);
+console.log(age * 5);
+console.log(age / 5);
+
+//10*10*10 10의 3승
+console.log(age ** 3);
+//3을 나눈 나머지 값.
+console.log(age % 3);
+
+//증감 연산자
+age++;
+console.log(age);
+age--;
+console.log(age);
+
+//복합대입 연산자
+age += 30;
+console.log(age);
+age -= 5;
+console.log(age);
